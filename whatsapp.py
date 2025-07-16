ed8cd63cc2ae968998e78599d9664c114eca80d2
gamboel99 <eko.w.a@students.kahuripan.ac.id>
2025-07-16 18:55:59 +0700

    Add files via upload

--- whatsapp.py
+++ whatsapp.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 def kirim_whatsapp(to, pesan, sid, token, from_wa):
     client = Client(sid, token)
     message = client.messages.create(
-        from_=f'whatsapp:{from_wa}',
+        from_=f"whatsapp:{from_wa}",
         body=pesan,
-        to=f'whatsapp:{to}'
+        to=f"whatsapp:{to}"
     )
     return message.sid
